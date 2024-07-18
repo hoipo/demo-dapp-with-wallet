2631923ba9a6814bebc87330f8824c03491fa256
potter.zhang <potter.zhang@bybit.com>
2024-07-18 14:35:30 +0800

    chore: dev

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import {Header} from "./components/Header/Header";
 import {TxForm} from "./components/TxForm/TxForm";
 
 function App() {
+  console.log('demo app started', (window as any).bybitTonWallet);
+
   return (
     <TonConnectUIProvider
       manifestUrl="https://ton-connect.github.io/demo-dapp-with-wallet/tonconnect-manifest.json"
