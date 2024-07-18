d4b38509521c068740569a0f5c7ab0ea709f115c
potter.zhang <potter.zhang@bybit.com>
2024-07-18 14:35:55 +0800

    chore: dev

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,8 @@ import {Header} from "./components/Header/Header";
 import {TxForm} from "./components/TxForm/TxForm";
 
 function App() {
-  console.log('demo app started', (window as any).bybitTonWallet);
+  console.log('demo app started');
+  console.log((window as any).bybitTonWallet);
 
   return (
     <TonConnectUIProvider
